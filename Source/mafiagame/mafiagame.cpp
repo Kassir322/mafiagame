9b86ac752727f2ff795d01844b08bd44184add3f
zelqer <sasha.sentsov@gmail.com>
2025-08-12 17:58:13 +0300

    1 commit

---
+++ Source/mafiagame/mafiagame.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "mafiagame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, mafiagame, "mafiagame" );
+ 
